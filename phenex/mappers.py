fe55dd3eeac19e740352507793ad2d2e060b592d
Stephen Privitera <stephen.privitera@bayer.com>
2025-01-21 15:11:40 +0100

    checkpointing

--- phenex/mappers.py
+++ phenex/mappers.py
@@ -3,113 +3,7 @@ from typing import Optional, Dict
 from dataclasses import dataclass, asdict
 from ibis.expr.types.relations import Table
 
-
-@dataclass
-class ColumnMapper:
-    """
-    The ColumnMapper class provides a template for mapping columns of a table from an arbitrary schema to an internal representation.
-
-    To subclass:
-        1. Check whether one of the existing ColumnMapper's fits your use case.
-        2. If not, define a new one by specifying the columns understood by the new ColumnMapper and adding those which are required to required_columns.
-
-    Attributes:
-        NAME_TABLE: The name of the table to be mapped.
-        required_columns: The columns which must always be specified when using the give ColumnMapper.
-
-    Methods:
-        rename(table: Table) -> Table:
-            Renames the columns of the given table according to the internal representation.
-    """
-
-    NAME_TABLE: str
-    required_columns = []
-
-    def rename(self, table: Table) -> Table:
-        """
-        Renames the columns of the given table according to the internal representation.
-
-        Args:
-            table (Table): The table to rename columns for.
-
-        Returns:
-            Table: The table with renamed columns.
-        """
-        mapping = copy.deepcopy(asdict(self))
-        mapping.pop("NAME_TABLE")
-        # delete optional params from mapping
-        for key in asdict(self).keys():
-            if key not in self.required_columns and getattr(self, key) is None:
-                del mapping[key]
-        return table.rename(**mapping)
-
-
-@dataclass
-class PersonTableColumnMapper(ColumnMapper):
-    """
-    Maps columns of a "person-like" table from an arbitrary schema to a PhenEx internal representation. A "person-like" table is a table that contains basic information about the patients in a database, generally characteristics that are time-independent (such as date of birth, race, sex at birth). These tables are required in the computation of PersonPhenotype's.
-
-    Attributes:
-        PERSON_ID (str): The column name for the person ID.
-        DATE_OF_BIRTH (str): The column name for the date of birth.
-    """
-
-    NAME_TABLE: str = "PERSON"
-    PERSON_ID: str = "PERSON_ID"
-    DATE_OF_BIRTH: Optional[str] = None
-    YEAR_OF_BIRTH: Optional[str] = None
-    DATE_OF_DEATH: Optional[str] = None
-    SEX: Optional[str] = None
-    ETHNICITY: Optional[str] = None
-    required_columns = ['PERSON_ID']
-
-
-@dataclass
-class CodeTableColumnMapper(ColumnMapper):
-    """
-    Maps columns of a code table from an arbitrary schema to an internal representation. A code table is a table that contains coded information about events or conditions related to patients, such as diagnoses, procedures, or medications. These tables typically include an event date, a code representing the event or condition, and optionally a code type.
-
-    Attributes:
-        EVENT_DATE (str): The column name for the event date.
-        CODE (str): The column name for the code.
-        CODE_TYPE (Optional[str]): The column name for the code type, if applicable.
-        PERSON_ID (str): The column name for the person ID.
-    """
-
-    NAME_TABLE: str
-    EVENT_DATE: str = "EVENT_DATE"
-    CODE: str = "CODE"
-    CODE_TYPE: Optional[str] = None
-    PERSON_ID: str = "PERSON_ID"
-
-    # some code tables do not have CODE_TYPE
-    required_columns = ['PERSON_ID', 'EVENT_DATE', 'CODE']
-
-
-@dataclass
-class MeasurementTableColumnMapper(CodeTableColumnMapper):
-    """
-    Maps columns of a measurement table from an arbitrary schema to an internal representation.A measurement table is a table that contains code information associated with a numerical value, for example lab test results.
-
-    Attributes:
-        EVENT_DATE (str): The column name for the event date.
-        CODE (str): The column name for the code.
-        CODE_TYPE (Optional[str]): The column name for the code type, if applicable.
-        PERSON_ID (str): The column name for the person ID.
-        VALUE (str): The column name for the value.
-    """
-
-    VALUE: str = "VALUE"
-    required_columns = ['PERSON_ID', 'EVENT_DATE', 'CODE', 'VALUE']
-
-
-@dataclass
-class ObservationPeriodTableMapper(ColumnMapper):
-    NAME_TABLE: str = "OBSERVATION_PERIOD"
-    PERSON_ID: str = "PERSON_ID"
-    OBSERVATION_PERIOD_START_DATE: str = "OBSERVATION_PERIOD_START_DATE"
-    OBSERVATION_PERIOD_END_DATE: str = "OBSERVATION_PERIOD_END_DATE"
-    required_columns = ['PERSON_ID', 'OBSERVATION_PERIOD_START_DATE', 'OBSERVATION_PERIOD_END_DATE']
+from phenex.tables import *
 
 
 class DomainsDictionary:
@@ -117,17 +11,17 @@ class DomainsDictionary:
     A DomainsDictionary is used to map an entire database from an arbitrary schema to a PhenEx internal representation.
 
     Attributes:
-        domains_dict (Dict[str, ColumnMapper]): A dictionary where keys are domain names and values are ColumnMapper instances.
+        domains_dict (Dict[str, ]): A dictionary where keys are domain names and values are  instances.
 
     Methods:
         get_mapped_tables(con, database=None) -> Dict[str, Table]:
             Get all tables mapped to PhenEx representation using the given connection.
     """
 
-    def __init__(self, domains_dict: Dict[str, ColumnMapper]):
+    def __init__(self, domains_dict):
         self.domains_dict = domains_dict
 
-    def get_mapped_tables(self, con, database=None) -> Dict[str, Table]:
+    def set_mapped_tables(self, con, overwrite=False) -> Dict[str, Table]:
         """
         Get all tables mapped to PhenEx representation using the given connection.
 
@@ -140,98 +34,154 @@ class DomainsDictionary:
         Returns:
             Dict[str, Table]: A dictionary where keys are domain names and values are mapped tables.
         """
+        existing_tables = con.dest_connection.list_tables(database=con.SNOWFLAKE_DEST_DATABASE)
+        for domain, mapper in self.domains_dict.items():
+            if domain not in existing_tables or overwrite:
+                t = con.source_connection.table(
+                    mapper.NAME_TABLE,
+                    database=con.SNOWFLAKE_SOURCE_DATABASE
+                )
+                mapped_table = mapper(t).table
+                con.dest_connection.create_view(
+                    name=mapper.NAME_TABLE,
+                    database=con.SNOWFLAKE_DEST_DATABASE,
+                    obj=mapped_table,
+                    overwrite=overwrite
+                )
+
+    def get_mapped_tables(self, con) -> Dict[str, Table]:
+        """
+        Get all tables mapped to PhenEx representation using the given connection.
+
+        If a database is not provided, the current database of the connection is used to find the tables.
+
+        Args:
+            con: The connection to the database.
+            database (Optional[str]): The name of the database. Defaults to the current database of the connection.
+
+        Returns:
+            Dict[str, Table]: A dictionary where keys are domain names and values are mapped tables.
+        """
+        # self.set_mapped_tables(con)
         mapped_tables = {}
         for domain, mapper in self.domains_dict.items():
-            t = con.get_source_table(
-                mapper.NAME_TABLE
-            )
-            mapped_tables[domain] = mapper.rename(t)
+            mapped_tables[domain] = mapper(con.dest_connection.table(
+                mapper.NAME_TABLE,
+                database=con.SNOWFLAKE_DEST_DATABASE
+            ))
         return mapped_tables
 
 
 #
 # OMOP Column Mappers
 #
-OMOPPersonTableColumnMapper = PersonTableColumnMapper(
-    NAME_TABLE="PERSON",
-    PERSON_ID="PERSON_ID",
-    DATE_OF_BIRTH="BIRTH_DATETIME",
-    YEAR_OF_BIRTH="YEAR_OF_BIRTH",
-    SEX="GENDER_CONCEPT_ID",
-    ETHNICITY="ETHNICITY_CONCEPT_ID",
-)
+class OMOPPersonTable(PhenexPersonTable):
+    JOIN_KEYS = {
+        'OMOPConditionOccurenceTable': ['PERSON_ID'],
+        'OMOPVisitDetailTable': ['PERSON_ID']
+    }
+    
+class OMOPVisitDetailTable(PhenexVisitDetailTable):
+    JOIN_KEYS = {
+        'OMOPPersonTable': ['PERSON_ID'],
+        'OMOPConditionOccurenceTable': ['PERSON_ID', 'VISIT_DETAIL_ID']
+    }
 
-OMOPDeathTableColumnMapper = PersonTableColumnMapper(
-    NAME_TABLE="DEATH", PERSON_ID="PERSON_ID", DATE_OF_DEATH="DEATH_DATE"
-)
-
-OMOPPersonTableSourceColumnMapper = PersonTableColumnMapper(
-    NAME_TABLE="PERSON",
-    PERSON_ID="PERSON_ID",
-    DATE_OF_BIRTH="BIRTH_DATETIME",
-    YEAR_OF_BIRTH="YEAR_OF_BIRTH",
-    SEX="GENDER_SOURCE_VALUE",
-    ETHNICITY="ETHNICITY_SOURCE_VALUE",
-)
-
-OMOPConditionOccurrenceColumnMapper = CodeTableColumnMapper(
-    NAME_TABLE="CONDITION_OCCURRENCE",
-    EVENT_DATE="CONDITION_START_DATE",
-    CODE="CONDITION_CONCEPT_ID",
-)
-
-OMOPConditionOccurrenceSourceColumnMapper = CodeTableColumnMapper(
-    NAME_TABLE="CONDITION_OCCURRENCE",
-    EVENT_DATE="CONDITION_START_DATE",
-    CODE="CONDITION_SOURCE_VALUE",
-)
-
-OMOPProcedureOccurrenceColumnMapper = CodeTableColumnMapper(
-    NAME_TABLE="PROCEDURE_OCCURRENCE",
-    EVENT_DATE="PROCEDURE_DATE",
-    CODE="PROCEDURE_CONCEPT_ID",
-)
-
-OMOPProcedureOccurrenceSourceColumnMapper = CodeTableColumnMapper(
-    NAME_TABLE="PROCEDURE_OCCURRENCE",
-    EVENT_DATE="PROCEDURE_DATE",
-    CODE="PROCEDURE_SOURCE_VALUE",
-)
-
-OMOPDrugExposureColumnMapper = CodeTableColumnMapper(
-    NAME_TABLE="DRUG_EXPOSURE",
-    EVENT_DATE="DRUG_EXPOSURE_START_DATE",
-    CODE="DRUG_CONCEPT_ID",
-)
-
-OMOPDrugExposureSourceColumnMapper = CodeTableColumnMapper(
-    NAME_TABLE="DRUG_EXPOSURE",
-    EVENT_DATE="DRUG_EXPOSURE_START_DATE",
-    CODE="DRUG_SOURCE_VALUE",
-)
-
-OMOPObservationPeriodColumnMapper = ObservationPeriodTableMapper(
-    NAME_TABLE="OBSERVATION_PERIOD",
-    PERSON_ID="PERSON_ID",
-    OBSERVATION_PERIOD_START_DATE="OBSERVATION_PERIOD_START_DATE",
-    OBSERVATION_PERIOD_END_DATE="OBSERVATION_PERIOD_END_DATE",
-)
-
-OMOPColumnMappers = {
-    "PERSON": OMOPPersonTableColumnMapper,
-    "DEATH": OMOPDeathTableColumnMapper,
-    "CONDITION_OCCURRENCE": OMOPConditionOccurrenceColumnMapper,
-    "PROCEDURE_OCCURRENCE": OMOPProcedureOccurrenceColumnMapper,
-    "DRUG_EXPOSURE": OMOPDrugExposureColumnMapper,
-    "PERSON_SOURCE": OMOPPersonTableSourceColumnMapper,
-    "CONDITION_OCCURRENCE_SOURCE": OMOPConditionOccurrenceSourceColumnMapper,
-    "PROCEDURE_OCCURRENCE_SOURCE": OMOPProcedureOccurrenceSourceColumnMapper,
-    "DRUG_EXPOSURE_SOURCE": OMOPDrugExposureSourceColumnMapper,
-    "OBSERVATION_PERIOD": OMOPObservationPeriodColumnMapper,
-}
-
+class OMOPConditionOccurenceTable(CodeTable):
+    NAME_TABLE = 'CONDITION_OCCURRENCE'
+    JOIN_KEYS = {
+        'OMOPPersonTable': ['PERSON_ID'],
+        'OMOPVisitDetailTable': ['PERSON_ID', 'VISIT_DETAIL_ID'] # I changed this from EVENT_DATE
+    }
+    DEFAULT_MAPPING = {
+        'PERSON_ID': "PERSON_ID",
+        'EVENT_DATE': "CONDITION_START_DATE",
+        'CODE': "CONDITION_CONCEPT_ID",
+    }
 
 #
 # Domains
 #
-OMOPDomains = DomainsDictionary(OMOPColumnMappers)
+OMOPs = {
+    "PERSON": OMOPPersonTable,
+    "VISIT": OMOPVisitDetailTable,
+    "CONDITION_OCCURRENCE": OMOPConditionOccurenceTable,
+    # "DEATH": OMOPDeathTable,
+    # "CONDITION_OCCURRENCE": OMOPConditionOccurrence,
+    # "PROCEDURE_OCCURRENCE": OMOPProcedureOccurrence,
+    # "DRUG_EXPOSURE": OMOPDrugExposure,
+    # "PERSON_SOURCE": OMOPPersonTableSource,
+    # "CONDITION_OCCURRENCE_SOURCE": OMOPConditionOccurrenceSource,
+    # "PROCEDURE_OCCURRENCE_SOURCE": OMOPProcedureOccurrenceSource,
+    # "DRUG_EXPOSURE_SOURCE": OMOPDrugExposureSource,
+    # "OBSERVATION_PERIOD": OMOPObservationPeriod,
+}
+OMOPDomains = DomainsDictionary(OMOPs)
+
+#
+# OLD vvvvv
+#
+# OMOPPersonTable = PersonTable(
+#     NAME_TABLE="PERSON",
+#     PERSON_ID="PERSON_ID",
+#     DATE_OF_BIRTH="BIRTH_DATETIME",
+#     YEAR_OF_BIRTH="YEAR_OF_BIRTH",
+#     SEX="GENDER_CONCEPT_ID",
+#     ETHNICITY="ETHNICITY_CONCEPT_ID",
+# )
+
+# OMOPDeathTable = PersonTable(
+#     NAME_TABLE="DEATH", PERSON_ID="PERSON_ID", DATE_OF_DEATH="DEATH_DATE"
+# )
+
+# OMOPPersonTableSource = PersonTable(
+#     NAME_TABLE="PERSON",
+#     PERSON_ID="PERSON_ID",
+#     DATE_OF_BIRTH="BIRTH_DATETIME",
+#     YEAR_OF_BIRTH="YEAR_OF_BIRTH",
+#     SEX="GENDER_SOURCE_VALUE",
+#     ETHNICITY="ETHNICITY_SOURCE_VALUE",
+# )
+
+# OMOPConditionOccurrence = CodeTable(
+#     NAME_TABLE="CONDITION_OCCURRENCE",
+#     EVENT_DATE="CONDITION_START_DATE",
+#     CODE="CONDITION_CONCEPT_ID",
+# )
+
+# OMOPConditionOccurrenceSource = CodeTable(
+#     NAME_TABLE="CONDITION_OCCURRENCE",
+#     EVENT_DATE="CONDITION_START_DATE",
+#     CODE="CONDITION_SOURCE_VALUE",
+# )
+
+# OMOPProcedureOccurrence = CodeTable(
+#     NAME_TABLE="PROCEDURE_OCCURRENCE",
+#     EVENT_DATE="PROCEDURE_DATE",
+#     CODE="PROCEDURE_CONCEPT_ID",
+# )
+
+# OMOPProcedureOccurrenceSource = CodeTable(
+#     NAME_TABLE="PROCEDURE_OCCURRENCE",
+#     EVENT_DATE="PROCEDURE_DATE",
+#     CODE="PROCEDURE_SOURCE_VALUE",
+# )
+
+# OMOPDrugExposure = CodeTable(
+#     NAME_TABLE="DRUG_EXPOSURE",
+#     EVENT_DATE="DRUG_EXPOSURE_START_DATE",
+#     CODE="DRUG_CONCEPT_ID",
+# )
+
+# OMOPDrugExposureSource = CodeTable(
+#     NAME_TABLE="DRUG_EXPOSURE",
+#     EVENT_DATE="DRUG_EXPOSURE_START_DATE",
+#     CODE="DRUG_SOURCE_VALUE",
+# )
+
+# OMOPObservationPeriod = ObservationPeriodTableMapper(
+#     NAME_TABLE="OBSERVATION_PERIOD",
+#     PERSON_ID="PERSON_ID",
+#     OBSERVATION_PERIOD_START_DATE="OBSERVATION_PERIOD_START_DATE",
+#     OBSERVATION_PERIOD_END_DATE="OBSERVATION_PERIOD_END_DATE",
+# )
--- phenex/tables.py
+++ phenex/tables.py
@@ -1,74 +1,277 @@
 from ibis.expr.types.relations import Table
 
+from typing import Union
+import ibis 
+from ibis.expr.types.relations import Table
+import copy
 
-class PersonTable(Table):
-    # These datatpyes are just used for type hinting
-    pass
+class PhenexTable:
+    """
+    Phenex provides certain table types on which it knows how to operate. For instance, Phenex implements a CodeTable, which is an event table containing codes. Phenex has abstracted operations for each table type. For instance, given a CodeTable, Phenex knows how to filter this table based on the presence of codes within that table. Phenex doesn't care if the code table is actually a diagnosis code table or a procedure code table or a medication code table. 
+    
+    In onboarding a new data model to Phenex, the tables must be mapped into Phenex table types by subclassing the appropriate PhenexTable. When subclassing a PhenexTable, you must define:
+    
+        1. COLUMN_MAPPING: a mapping of the input table columns to the fields on the chosen PhenexTable type (e.g. 'CD' maps to 'CODE' in a CodeTable).
+        2. JOIN_KEYS: if you want to use the autojoin functionality of PhenexTable, you must specify what keys to use to join pairs of tables
+        3. PATHS: if you want to use the autojoin functionality of PhenexTable for more complex joins, you must specify join paths to take to get from one table to another    
+
+    Note that for each table type, there are REQUIRED_FIELDS, i.e., fields that MUST be defined for Phenex to work with such a table and KNOWN_FIELDS, i.e., fields that Phenex internally understands what to do with (there is a Phenotype that knows how to work with that field). For instance, in a PhenexPersonTable, one MUST define PERSON_ID, but DATE_OF_BIRTH is an optional field that PhenEx can process if given and transform into AGE. These are fixed for each table type and should not be overridden.
+    """
+    NAME_TABLE = 'PHENEX_TABLE' # name of table in the database
+    JOIN_KEYS = {} # dict: class name -> List[phenex column names]
+    KNOWN_FIELDS = [] # List[phenex column names]
+    DEFAULT_MAPPING = {} # dict: input column name -> phenex column name
+    PATHS = {} # dict: table class name -> List[other table class names]
+    
+    def __init__(self, table, name=None, column_mapping={}):
+        '''
+        Instantiate a PhenexTable, possibly overriding NAME_TABLE and COLUMN_MAPPING.
+        '''
+
+        if not isinstance(table, Table):
+            raise TypeError(f"Cannot instantiatiate {self.__class__.__name__} from {type(table)}. Must be ibis Table.")
+
+        self.NAME_TABLE = name or self.NAME_TABLE
+
+        self.column_mapping = self._get_column_mapping(column_mapping)
+        self._table = table.mutate(**self.column_mapping)
+
+        for key in self.REQUIRED_FIELDS:
+            try:
+                getattr(self._table, key)
+            except AttributeError:
+                raise ValueError(f"Required field {key} not defined in COLUMN_MAPPING.")
+
+        self._add_phenotype_table_relationship()
+
+    def _add_phenotype_table_relationship(self):
+        self.JOIN_KEYS['PhenotypeTable'] = ['PERSON_ID']
+
+    def _get_column_mapping(self, column_mapping=None):
+        column_mapping = column_mapping or {}
+        for key in column_mapping.keys():
+            if key not in self.KNOWN_FIELDS:
+                raise ValueError(f"Unknown mapped field {key} --> {column_mapping[key]} for f{type(self)}.")
+        default_mapping = copy.deepcopy(self.DEFAULT_MAPPING)
+        default_mapping.update(column_mapping)
+        return default_mapping
+        
+    def __getattr__(self, name):
+        # pass all attributes on to underlying table
+        return getattr(self._table, name)
+
+    @classmethod
+    def REQUIRED_FIELDS(cls):
+        return list(cls.DEFAULT_MAPPING.keys())
+    
+    @property
+    def table(self):
+        return self._table
+
+    def join(self, other: 'PhenexTable', *args, domains = None, **kwargs):
+        """
+        The join method performs a join of PhenexTables, using autojoin functionality if Phenex is able to find the table types specified in PATHS.
+        """
+        if not isinstance(other, PhenexTable):
+            raise TypeError(f"Expected a PhenexTable instance, got {type(other)}")
+        if len(args):
+            # if user specifies join keys and join type, simply perform join as specified
+            return type(self)(self.table.join(other.table, *args, **kwargs))
+
+        # Do an autojoin by finding a path from the left to the right table and sequentially joining as necessary            
+        joined_table = current_table = self        
+        for next_table_class_name in self._find_path(other):
+            next_table = [v for k,v in domains.items() if v.__class__.__name__ == next_table_class_name]
+            if len(next_table) != 1:
+                raise ValueError(f"Unable to find unqiue {next_table_class_name} required to join {other.__class__.__name__}")
+
+            next_table = next_table[0]
+            print(f"Joining : {current_table.__class__.__name__} to {next_table.__class__.__name__}")
+
+            # join keys are defined by the left table; in theory should enforce symmetry
+            join_keys = current_table.JOIN_KEYS[next_table_class_name] 
+            columns = list(set(joined_table.columns + next_table.columns))
+            joined_table = joined_table.join(next_table, join_keys, **kwargs).select(columns)
+            current_table = next_table
+        
+        return joined_table
+
+        
+    def _find_path(self, other):
+
+        start_name = self.__class__.__name__
+        end_name = other.__class__.__name__
+        # first see if direct connection
+        try:
+            self.JOIN_KEYS[end_name]
+            return [end_name]
+        except KeyError:
+            try:
+                return self.PATHS[end_name] + [end_name]
+            except KeyError:
+                raise ValueError(f'Cannot autojoin {start_name} --> {end_name}. Please specify join path in PATHS.')
+
+    def select(self, *args, **kwargs):
+        return type(self)(self.table.select(*args, **kwargs), name=self.NAME_TABLE)
+    
+    def filter(self, expr):
+        '''
+        Filter the table by an Ibis Expression or using a PhenExFilter.
+        '''
+        input_columns = self.columns
+        if isinstance(expr, ibis.expr.types.Expr):
+            filtered_table = self.table.filter(expr)
+        else:
+            filtered_table = expr.filter(self)
+            
+        return type(self)(
+            filtered_table.select(input_columns), 
+            name=self.NAME_TABLE, 
+            column_mapping=self.column_mapping)
+    
+
+class PhenexPersonTable(PhenexTable):
+    
+    NAME_TABLE = 'PERSON'
+    JOIN_KEYS = {
+        'CodeTable': ['PERSON_ID'],
+        'PhenexVisitDetailTable': ['PERSON_ID']
+    }
+    KNOWN_FIELDS = [
+        'PERSON_ID',
+        'DATE_OF_BIRTH', 
+        'YEAR_OF_BIRTH', 
+        'DATE_OF_DEATH',
+        'SEX', 
+        'ETHNICITY'
+    ]
+    DEFAULT_MAPPING = {
+        'PERSON_ID': "PERSON_ID"
+    }
+
+class EventTable(PhenexTable):
+    
+    NAME_TABLE = 'EVENT'
+    KNOWN_FIELDS = [
+        'PERSON_ID',
+        'EVENT_DATE'
+    ]
+    DEFAULT_MAPPING = {
+        'PERSON_ID': "PERSON_ID",
+        'EVENT_DATE': "EVENT_DATE",
+    }
+
+class CodeTable(PhenexTable):
+    
+    NAME_TABLE = 'CODE'
+    RELATIONSHIPS = {
+        'PhenexPersonTable': ['PERSON_ID'],
+        'PhenexVisitDetailTable': ['PERSON_ID', 'VISIT_DETAIL_ID'],
+    }
+    KNOWN_FIELDS = [
+        'PERSON_ID',
+        'EVENT_DATE', 
+        'CODE',
+        'CODE_TYPE',
+        'VISIT_DETAIL_ID'
+    ]
+    DEFAULT_MAPPING = {
+        'PERSON_ID': "PERSON_ID",
+        'EVENT_DATE': "EVENT_DATE",
+        'CODE': "CODE",
+    }
+
+class PhenexVisitDetailTable(PhenexTable):
+    
+    NAME_TABLE = 'VISIT_DETAIL'
+    RELATIONSHIPS = {
+        'PhenexPersonTable': ['PERSON_ID'],
+        'CodeTable': ['PERSON_ID', 'VISIT_DETAIL_ID'],
+    }
+    KNOWN_FIELDS = [
+        'PERSON_ID',
+        'VISIT_DETAIL_ID',
+        'VISIT_DETAIL_SOURCE_VALUE',
+    ]
+    
+    DEFAULT_MAPPING = {
+        'PERSON_ID':'PERSON_ID',
+        'VISIT_DETAIL_ID': 'VISIT_DETAIL_ID',
+        'VISIT_DETAIL_SOURCE_VALUE': 'VISIT_DETAIL_SOURCE_VALUE'
+    }
 
 
-class IndexTable(Table):
-    # These datatpyes are just used for type hinting
-    pass
-
-
-class CodeTable(Table):
-    # These datatpyes are just used for type hinting
-    pass
-
-
-class EventTable(Table):
-    # These datatpyes are just used for type hinting
-    pass
-
+class PhenexIndexTable(PhenexTable):
+   
+    NAME_TABLE = 'INDEX'
+    JOIN_KEYS = {
+        'CodeTable': ['PERSON_ID'],
+        'PhenexVisitDetailTable': ['PERSON_ID']
+    }
+    KNOWN_FIELDS = [
+        'PERSON_ID',
+        'INDEX_DATE',
+    ]
+    DEFAULT_MAPPING = {
+        'PERSON_ID': "PERSON_ID",
+        "INDEX_DATE": "INDEX_DATE"
+    }
 
 class MeasurementTable(Table):
     # These datatpyes are just used for type hinting
     pass
 
 
-class PhenotypeTable(Table):
-    # These datatpyes are just used for type hinting
-    pass
+class PhenotypeTable(PhenexTable):
+    NAME_TABLE = 'PHENOTYPE'
+    KNOWN_FIELDS = [
+        'PERSON_ID',
+        'BOOLEAN',
+        'DATE',
+        'VALUE'
+    ]
+    DEFAULT_MAPPING = {
+        'PERSON_ID': "PERSON_ID",
+        "BOOLEAN": "BOOLEAN",
+        "DATE": "DATE",
+        "VALUE": "VALUE"
+    }
 
 
-PERSON_TABLE_COLUMNS = ["PERSON_ID"]
-CODE_TABLE_COLUMNS = ["PERSON_ID", "EVENT_DATE", "CODE"]
-PHENOTYPE_TABLE_COLUMNS = ["PERSON_ID", "BOOLEAN", "EVENT_DATE", "VALUE"]
-EVENT_TABLE_COLUMNS = ["PERSON_ID", "EVENT_DATE"]
-INDEX_TABLE_COLUMNS = ["PERSON_ID", "INDEX_DATE"]
-
-
-def is_phenex_person_table(table: Table) -> bool:
+def is_phenex_person_table(table: PhenexTable) -> bool:
     """
     Check if given table is a person table.
     One could check one row per patient?
     """
-    return set(PERSON_TABLE_COLUMNS) <= set(table.columns)
+    return isinstance(table, PhenexPersonTable)
 
 
-def is_phenex_code_table(table: Table) -> bool:
+def is_phenex_code_table(table: PhenexTable) -> bool:
     """
     Check if given table is a code table.
     """
-    return set(CODE_TABLE_COLUMNS) <= set(table.columns + ["CODE_TYPE"])
+    return isinstance(table, CodeTable)
 
 
-def is_phenex_event_table(table: Table) -> bool:
+def is_phenex_event_table(table: PhenexTable) -> bool:
     """
     Check if given table is a code table.
     """
-    return set(EVENT_TABLE_COLUMNS) <= set(table.columns)
+    return isinstance(table, CodeTable)
 
 
-def is_phenex_phenotype_table(table: Table) -> bool:
+def is_phenex_phenotype_table(table: PhenexTable) -> bool:
     """
     Check if given table is a code table.
     """
-    return set(PHENOTYPE_TABLE_COLUMNS) <= set(table.columns)
+    return isinstance(table, PhenotypeTable)
 
 
-def is_phenex_index_table(table: Table) -> bool:
+def is_phenex_index_table(table: PhenexTable) -> bool:
     """
     Check if given table is a code table.
     """
-    return set(INDEX_TABLE_COLUMNS) <= set(table.columns)
+    return isinstance(table, PhenexIndexTable)
+
+
+PHENOTYPE_TABLE_COLUMNS = PhenotypeTable.REQUIRED_FIELDS
