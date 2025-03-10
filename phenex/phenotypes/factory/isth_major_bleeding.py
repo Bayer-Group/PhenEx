40533daf90bb0e1c04580e7f4284f1c2a7ae7800
a-hartens <alexander.hartenstein@bayer.com>
2025-03-10 16:03:11 +0100

    compiles

--- phenex/phenotypes/factory/isth_major_bleeding.py
+++ phenex/phenotypes/factory/isth_major_bleeding.py
@@ -10,42 +10,49 @@ from phenex.filters import (
     CategoricalFilter,
 )
 
+
 @dataclass
 class ISTHBleedComponents:
     critical_organ_bleed_codelist: Codelist
     overt_bleed_codelist: Codelist
     possible_bleed_codelist: Codelist
+    transfusion_codelist: Codelist
     inpatient: CategoricalFilter
     outpatient: CategoricalFilter
     primary_diagnosis: CategoricalFilter
     secondary_diagnosis: CategoricalFilter
     diagnosis_of: CategoricalFilter = None
-    diagnosis_code_domain: str = 'CONDITION_OCCURRENCE_SOURCE'
-    procedure_code_domain: str = 'PROCEDURE_OCCURRENCE_SOURCE'
-    death_domain: str = 'PERSON'
-    anchor_phenotype: Optional[Union[str, "Phenotype"]] = None
-    when: str = 'after'
+    diagnosis_code_domain: str = "CONDITION_OCCURRENCE_SOURCE"
+    procedure_code_domain: str = "PROCEDURE_OCCURRENCE_SOURCE"
+    death_domain: str = "PERSON"
 
     @property
-    def relative_time_range(self):
-        return  RelativeTimeRangeFilter(
-            when = self.when,
-            anchor_phenotype = self.anchor_phenotype
-        )
-    
-    @property
-    def component_phenotype_return_date(self):
-        if self.when == 'after':
-            return 'first'
-        return 'last'
+    def component_phenotype_date_select(self):
+        if self.when == "after":
+            return "first"
+        return "last"
+
 
 def ISTHMajorBleedingPhenotype(
-        components: ISTHBleedComponents,
-        return_date : str = 'first'
+    components: ISTHBleedComponents,
+    return_date: str = "first",
+    relative_time_range: Optional[RelativeTimeRangeFilter] = None,
 ) -> LogicPhenotype:
-    critical_organ_bleed = CriticalOrganBleedPhenotype(components)
-    symptomatic_bleed = SymptomaticBleedPhenotype(components)
-    fatal_bleed = FatalBleedPhenotype(components)
+    critical_organ_bleed = CriticalOrganBleedPhenotype(
+        components=components,
+        relative_time_range=relative_time_range,
+        return_date=return_date,
+    )
+    symptomatic_bleed = SymptomaticBleedPhenotype(
+        components=components,
+        relative_time_range=relative_time_range,
+        return_date=return_date,
+    )
+    fatal_bleed = FatalBleedPhenotype(
+        components=components,
+        relative_time_range=relative_time_range,
+        return_date=return_date,
+    )
     return LogicPhenotype(
         name="isth_major_bleed",
         expression=critical_organ_bleed | symptomatic_bleed | fatal_bleed,
@@ -53,81 +60,107 @@ def ISTHMajorBleedingPhenotype(
     )
 
 
-def CriticalOrganBleedPhenotype(c:ISTHBleedComponents):
+def CriticalOrganBleedPhenotype(
+    components: ISTHBleedComponents,
+    return_date: str = "first",
+    relative_time_range: Optional[RelativeTimeRangeFilter] = None,
+):
     # create required categorical filters; critical organ bleed occurs only in the inpatient position and can be either primary or secondary diagnosis position
-    categorical_filters = c.inpatient & (c.primary_diagnosis | c.secondary_diagnosis)
-    categorical_filters = add_diagnosis_of_filter(categorical_filters, c)
+    categorical_filters = components.inpatient & (components.primary_diagnosis | components.secondary_diagnosis)
+    categorical_filters = add_diagnosis_of_filter(categorical_filters, components)
 
     return CodelistPhenotype(
-        name = 'isth_critical_organ_bleed',
-        domain = c.diagnosis_code_domain,
-        codelist = c.critical_organ_bleed_codelist,
-        categorical_filter = categorical_filters,
-        relative_time_range=c.relative_time_range,
-        return_date=c.component_phenotype_return_date
+        name="isth_critical_organ_bleed",
+        domain=components.diagnosis_code_domain,
+        codelist=components.critical_organ_bleed_codelist,
+        categorical_filter=categorical_filters,
+        relative_time_range=relative_time_range,
+        return_date=component_return_date(relative_time_range),
     )
 
 
-def SymptomaticBleedPhenotype(c:ISTHBleedComponents):
+def SymptomaticBleedPhenotype(
+    components: ISTHBleedComponents,
+    return_date: str = "first",
+    relative_time_range: Optional[RelativeTimeRangeFilter] = None,
+):
     # create required categorical filters; critical organ bleed occurs only in the inpatient position and can be either primary or secondary diagnosis position
-    categorical_filters = c.inpatient & (c.primary_diagnosis | c.secondary_diagnosis)
-    categorical_filters = add_diagnosis_of_filter(categorical_filters, c)
+    categorical_filters = components.inpatient & (components.primary_diagnosis | components.secondary_diagnosis)
+    categorical_filters = add_diagnosis_of_filter(categorical_filters, components)
 
     overt_bleed = CodelistPhenotype(
-        name = 'isth_overt_bleed',
-        domain = c.diagnosis_code_domain,
-        codelist = c.overt_bleed_codelist,
-        categorical_filter = categorical_filters,
-        relative_time_range=c.relative_time_range,
-        return_date=c.component_phenotype_return_date
+        name="isth_overt_bleed",
+        domain=components.diagnosis_code_domain,
+        codelist=components.overt_bleed_codelist,
+        categorical_filter=categorical_filters,
+        relative_time_range=relative_time_range,
+        return_date=component_return_date(relative_time_range),
     )
 
-    return BleedVerificationPhenotype(overt_bleed)
+    return BleedVerificationPhenotype(overt_bleed, components, relative_time_range=relative_time_range)
+
+def component_return_date(relative_time_range):
+    if relative_time_range.when == "after":
+        return "first"
+    return "last"
 
 
-
-def FatalBleedPhenotype(c:ISTHBleedComponents):
-    death = DeathPhenotype(relative_time_range=c.relative_time_range, domain = c.death_domain)
+def FatalBleedPhenotype(components: ISTHBleedComponents, 
+                        return_date: str = "first",
+                        relative_time_range: Optional[RelativeTimeRangeFilter] = None
+                        ):
+    death = DeathPhenotype(
+        relative_time_range=relative_time_range, domain=components.death_domain
+    )
 
     relative_45_days_prior = RelativeTimeRangeFilter(
-        when='before',
-        min_days = GreaterThanOrEqualTo(0),
-        max_days = LessThanOrEqualTo(45),
-        anchor_phenotype=death
+        when="before",
+        min_days=GreaterThanOrEqualTo(0),
+        max_days=LessThanOrEqualTo(45),
+        anchor_phenotype=death,
     )
 
     # create required categorical filters; critical organ bleed already covers the inpatient conditions. we must only look for outpatient and primary diagnosis positions
-    categorical_filters = c.outpatient & c.primary_diagnosis
-    categorical_filters = add_diagnosis_of_filter(categorical_filters, c)
+    categorical_filters = components.outpatient & components.primary_diagnosis
+    categorical_filters = add_diagnosis_of_filter(categorical_filters, components=components)
 
     return CodelistPhenotype(
-        name = 'isth_fatal_bleed',
-        domain = c.diagnosis_code_domain,
-        codelist = c.critical_organ_bleed_codelist,
-        categorical_filter = categorical_filters,
-        relative_time_range=c.relative_time_range & relative_45_days_prior
-        return_date=c.component_phenotype_return_date
+        name="isth_fatal_bleed",
+        domain=components.diagnosis_code_domain,
+        codelist=components.critical_organ_bleed_codelist,
+        categorical_filter=categorical_filters,
+        relative_time_range=[relative_time_range, relative_45_days_prior],
+        return_date=component_return_date(relative_time_range),
     )
 
-def add_diagnosis_of_filter(categorical_filters, c):
+
+def add_diagnosis_of_filter(categorical_filters, components):
     # if a 'diagnosis_of' (as opposed to 'history_of', etc) categorical filter is provided, add it to the list of conditions
-    if c.diagnosis_of is not None:
-        categorical_filters = categorical_filters & c.diagnosis_of 
+    if components.diagnosis_of is not None:
+        categorical_filters = categorical_filters & components.diagnosis_of
     return categorical_filters
 
 
-def BleedVerificationPhenotype(anchor_phenotype, c:ISTHBleedComponents):
+def BleedVerificationPhenotype(
+        anchor_phenotype, 
+        components: ISTHBleedComponents, 
+        relative_time_range: Optional[RelativeTimeRangeFilter] = None
+    ):
+
     within_two_days = RelativeTimeRangeFilter(
-        when='after',
-        min_days = GreaterThanOrEqualTo(-2),
-        max_days = LessThanOrEqualTo(2),
-        anchor_phenotype=anchor_phenotype
+        when="after",
+        min_days=GreaterThanOrEqualTo(-2),
+        max_days=LessThanOrEqualTo(2),
+        anchor_phenotype=anchor_phenotype,
     )
+    if relative_time_range is not None:
+        relative_time_range = [relative_time_range, within_two_days]
+    
     transfusion = CodelistPhenotype(
-        name = 'transfusion',
-        codelist = c.transfusion_codelist,
-        domain = c.procedure_domain,
-        relative_time_range=anchor_phenotype
+        name="transfusion_two_days_within_"+anchor_phenotype.name,
+        codelist=components.transfusion_codelist,
+        domain=components.procedure_code_domain,
+        relative_time_range=relative_time_range,
     )
 
     return transfusion
--- phenex/test/phenotypes/factory/test_isth_major.py
+++ phenex/test/phenotypes/factory/test_isth_major.py
@@ -3,6 +3,7 @@ import pandas as pd
 import ibis
 from phenex.test.cohort_test_generator import CohortTestGenerator
 from phenex.codelists import Codelist
+from phenex.phenotypes import Cohort, CodelistPhenotype
 from phenex.phenotypes.factory.isth_major_bleeding import (
     ISTHMajorBleedingPhenotype, ISTHBleedComponents
 )
@@ -21,64 +22,110 @@ from phenex.test.cohort.test_mappings import (
     ConditionOccurenceTableForTests,
 )
 
+from phenex.mappers import OMOPPersonTableSource, OMOPProcedureOccurrenceSourceTable, OMOPConditionOccurrenceSourceTable, OMOPDeathTable
+
 from phenex.test.util.dummy.generate_dummy_cohort_data import generate_dummy_cohort_data
 
 
 class ISTHTestGenerator(CohortTestGenerator):
     def define_cohort(self):
+        entry = CodelistPhenotype(
+            name='entry',
+            codelist = Codelist(["d0"]),
+            domain = 'CONDITION_OCCURRENCE',
+            return_date="first",
+        )
 
         c = ISTHBleedComponents(
-            critical_organ_bleed_codelist=Codelist(["d1"]).resolve(ignore_code_type=True),
-            overt_bleed_codelist=Codelist(["d2"]).resolve(ignore_code_type=True),
-            possible_bleed_codelist=Codelist(["d3"]).resolve(ignore_code_type=True),
-            inpatient=CategoricalFilter(column="ENCOUNTER_TYPE", allowed_values=['inpatient'], domain="VISIT_OCCURRENCE"),
-            outpatient=CategoricalFilter(column="ENCOUNTER_TYPE", allowed_values=['outpatient'], domain="VISIT_OCCURRENCE"),
-            primary_diagnosis=CategoricalFilter(column="DIAGNOSIS_POSITION", allowed_values=[1], domain="CONDITION_OCCURRENCE"),
-            secondary_diagnosis=~CategoricalFilter(column="DIAGNOSIS_POSITION", allowed_values=[1], domain="CONDITION_OCCURRENCE"),
-            diagnosis_of = CategoricalFilter(column="STATUS", allowed_values=['DIAGNOSIS_OF'], domain="CONDITION_OCCURRENCE"),
+            critical_organ_bleed_codelist=Codelist(["d1"]),
+            overt_bleed_codelist=Codelist(["d2"]),
+            possible_bleed_codelist=Codelist(["d3"]),
+            transfusion_codelist = Codelist(['t1']),
+            
+            inpatient = CategoricalFilter(column_name="ENCOUNTER_TYPE", allowed_values=['inpatient'], domain="VISIT_OCCURRENCE"),
+            outpatient = CategoricalFilter(column_name="ENCOUNTER_TYPE", allowed_values=['outpatient'], domain="VISIT_OCCURRENCE"),
+            primary_diagnosis = CategoricalFilter(column_name="DIAGNOSIS_POSITION", allowed_values=[1], domain="CONDITION_OCCURRENCE"),
+            secondary_diagnosis = ~CategoricalFilter(column_name="DIAGNOSIS_POSITION", allowed_values=[1], domain="CONDITION_OCCURRENCE"),
+            diagnosis_of = CategoricalFilter(column_name="STATUS", allowed_values=['DIAGNOSIS_OF'], domain="CONDITION_OCCURRENCE"),
+            
             diagnosis_code_domain = 'CONDITION_OCCURRENCE_SOURCE',
             procedure_code_domain = 'PROCEDURE_OCCURRENCE_SOURCE',
-            death_domain = 'PERSON',
-
+            death_domain = 'DEATH',
         )
 
-
-    inpatient: CategoricalFilter
-    outpatient: CategoricalFilter
-    primary_diagnosis: CategoricalFilter
-    secondary_diagnosis: CategoricalFilter
-    diagnosis_of: CategoricalFilter = None
-    diagnosis_code_domain: str = 'CONDITION_OCCURRENCE_SOURCE'
-    procedure_code_domain: str = 'PROCEDURE_OCCURRENCE_SOURCE'
-    death_domain: str = 'PERSON'
-    anchor_phenotype: Optional[Union[str, "Phenotype"]] = None
-    when: str = 'after'
-        entry = ISTHMajorBleedingPhenotype(
+        isth_bleeding = ISTHMajorBleedingPhenotype(
             return_date="first",
+            relative_time_range = RelativeTimeRangeFilter(when='after', anchor_phenotype=entry),
             components=c,
         )
-
 
         return Cohort(
             name="isth_major_bleed",
             entry_criterion=entry,
+            inclusion=[isth_bleeding]
         )
 
     def generate_dummy_input_data(self):
+
         values = [
             {
                 "name": "entry",
-                "values": ["d1", "d4"],
+                "values": ["d0"],
             },
             {
                 "name": "entry_date",
                 "values": [datetime.date(2020, 1, 1)],
             },
-            {"name": "prior_et_use", "values": ["e1", "e4"]},
             {
-                "name": "prior_et_use_date",
-                "values": [datetime.date(2019, 4, 1)],
+                "name": "d1",
+                "values": ["d1"],
             },
+            {
+                "name": "d1date",
+                "values": [datetime.date(2020, 2, 1), datetime.date(2019, 12, 1)],
+            },
+            {
+                "name": "d2",
+                "values": ["d2"],
+            },
+            {
+                "name": "d2date",
+                "values": [datetime.date(2020, 3, 1), datetime.date(2019, 11, 1)],
+            },
+            {
+                "name": "d3",
+                "values": ["d3"],
+            },
+            {
+                "name": "d3date",
+                "values": [datetime.date(2020, 4, 1), datetime.date(2019, 10, 1)],
+            },
+            {
+                "name": "death",
+                "values": [datetime.date(2020, 1, 15), datetime.date(2020, 2, 15), None],
+            },
+            {
+                "name": "transfusion",
+                "values": ["t1", "nt1"],
+            },
+            {
+                "name": "transfusion_date",
+                "values": [datetime.date(2020, 4, 2), None, datetime.date(2020, 4, 5)],
+            },
+            {
+                "name": "encounter_type",
+                "values": ["inpatient", "outpatient"],
+            },
+            {
+                "name": "diagnosis_position",
+                "values": [1, 2],
+            },
+            {
+                "name": "diagnosis_status",
+                "values": ['diagnosis_of', 'history_of'],
+            },
+
+
         ]
 
         return generate_dummy_cohort_data(values)
@@ -88,40 +135,54 @@ class ISTHTestGenerator(CohortTestGenerator):
         df_allvalues = self.generate_dummy_input_data()
 
         # create dummy person table
-        df_person = pd.DataFrame(df_allvalues[["PATID"]])
-        df_person["YOB"] = 1
-        df_person["GENDER"] = 1
-        df_person["ACCEPTABLE"] = 1
-        schema_person = {"PATID": str, "YOB": int, "GENDER": int, "ACCEPTABLE": int}
-        person_table = PersonTableForTests(
+        df_person = pd.DataFrame(df_allvalues[["PERSON_ID", 'death']])
+        schema_person = {"PERSON_ID": str, "death": datetime.date}
+        person_table = OMOPPersonTableSource(
             self.con.create_table("PERSON", df_person, schema=schema_person)
         )
-        # create drug exposure table
-        df_drug_exposure_entry = pd.DataFrame(
-            df_allvalues[["PATID", "entry", "entry_date"]]
-        )
-        df_drug_exclusion_exposure = pd.DataFrame(
-            df_allvalues[["PATID", "prior_et_use", "prior_et_use_date"]]
-        )
-        df_drug_exposure_entry.columns = ["PATID", "PRODCODEID", "ISSUEDATE"]
-        df_drug_exclusion_exposure.columns = ["PATID", "PRODCODEID", "ISSUEDATE"]
-        df_drug_exposure = pd.concat(
-            [df_drug_exposure_entry, df_drug_exclusion_exposure]
+
+        # create condition occurrence df
+        constant_columns = ['encounter_type', 'diagnosis_position', 'diagnosis_status']
+        code_sets = ['d1', 'd2', 'd3']
+        date_sets = ['d1date', 'd2date', 'd3date']
+        dfs = []
+        for code, date in zip(code_sets, date_sets):
+            _df = df_allvalues[['PERSON_ID', code, date]+constant_columns]
+            _df.columns = ["PERSON_ID", "CONDITION_SOURCE_VALUE", "CONDITION_START_DATE", "ENCOUNTER_TYPE", "DIAGNOSIS_POSITION", "STATUS"]
+            dfs.append(_df)
+        df_condition_occurrence = pd.concat(dfs)
+
+        # create condition occurrence table
+        schema_condition_occurrence = {
+            "PERSON_ID": str,
+            "CONDITION_SOURCE_VALUE": str,
+            "CONDITION_START_DATE": datetime.date,
+        }
+        condition_occurrence_table = OMOPConditionOccurrenceSourceTable(
+            self.con.create_table(
+                "CONDITION_OCCURRENCE_SOURCE", df_condition_occurrence, schema=schema_condition_occurrence
+            )
         )
 
-        schema_drug_exposure = {
-            "PATID": str,
-            "PRODCODEID": str,
-            "ISSUEDATE": datetime.date,
+        df_procedure_occurrence = pd.DataFrame(df_allvalues[["PERSON_ID", 'transfusion', 'transfusion_date']])
+        df_procedure_occurrence.columns = ["PERSON_ID", "PROCEDURE_SOURCE_VALUE", "PROCEDURE_DATE"]
+
+        # create procedure occurrence table
+        schema_procedure_occurrence = {
+            "PERSON_ID": str,
+            "PROCEDURE_SOURCE_VALUE": str,
+            "PROCEDURE_DATE": datetime.date,
         }
-        drug_exposure_table = DrugExposureTableForTests(
+        procedure_occurrence_table = OMOPProcedureOccurrenceSourceTable(
             self.con.create_table(
-                "DRUG_EXPOSURE", df_drug_exposure, schema=schema_drug_exposure
+                "CONDITION_OCCURRENCE_SOURCE", df_procedure_occurrence, schema=schema_procedure_occurrence
             )
         )
         return {
             "PERSON": person_table,
-            "DRUG_EXPOSURE": drug_exposure_table,
+            "DEATH": person_table,
+            "CONDITION_OCCURRENCE_SOURCE": condition_occurrence_table,
+            "PROCEDURE_OCCURRENCE_SOURCE": procedure_occurrence_table,
         }
 
     def define_expected_output(self):
