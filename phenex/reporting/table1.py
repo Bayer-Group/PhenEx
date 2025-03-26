2d543ded44ed3c299d9bfdf3d106dc0d223a835e
a-hartens <alexander.hartenstein@bayer.com>
2025-03-26 10:40:36 +0100

    black and logging fix

--- phenex/reporting/table1.py
+++ phenex/reporting/table1.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
 from phenex.reporting.reporter import Reporter
-from phenex.util.logging import getLoo logging
+from phenex.util import create_logger
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+logger = create_logger(__name__)
+
 
 class Table1(Reporter):
     """
@@ -24,38 +24,49 @@ class Table1(Reporter):
             .count()
             .execute()
         )
-
+        logger.debug("Starting with value columns for table1")
         self.df_booleans = self._report_boolean_columns()
+        logger.debug("Starting with boolean columns for table1")
         self.df_values = self._report_value_columns()
 
         # add percentage column
         if self.df_booleans is not None and self.df_values is not None:
             self.df = pd.concat([self.df_booleans, self.df_values])
         else:
-            self.df = self.df_booleans if self.df_booleans is not None else self.df_values
+            self.df = (
+                self.df_booleans if self.df_booleans is not None else self.df_values
+            )
         self.df["%"] = 100 * self.df["N"] / self.N
 
         # reorder columns so N and % are first
         first_cols = ["N", "%"]
         column_order = first_cols + [x for x in self.df.columns if x not in first_cols]
         self.df = self.df[column_order]
+        logger.debug("Finished creating table1")
         return self.df
 
     def _get_boolean_characteristics(self):
-        return [x for x in self.cohort.characteristics if type(x).__name__ not in ['MeasurementPhenotype', 'AgePhenotype']]
-    
+        return [
+            x
+            for x in self.cohort.characteristics
+            if type(x).__name__ not in ["MeasurementPhenotype", "AgePhenotype"]
+        ]
+
     def _get_value_characteristics(self):
-        return [x for x in self.cohort.characteristics if type(x).__name__ not in ['MeasurementPhenotype', 'AgePhenotype']]
+        return [
+            x
+            for x in self.cohort.characteristics
+            if type(x).__name__ not in ["MeasurementPhenotype", "AgePhenotype"]
+        ]
 
     def _report_boolean_columns(self):
         table = self.cohort.characteristics_table
         # get list of all boolean columns
         boolean_phenotypes = self._get_boolean_characteristics()
         boolean_columns = list(set([f"{x.name}_BOOLEAN" for x in boolean_phenotypes]))
-        logger.debug(f'Found {len(boolean_columns)} : {boolean_columns}')
+        logger.debug(f"Found {len(boolean_columns)} : {boolean_columns}")
         if len(boolean_columns) == 0:
             return None
-        
 
         # get count of 'Trues' in the boolean columns i.e. the phenotype counts
         true_counts = [
@@ -80,8 +91,8 @@ class Table1(Reporter):
         # get value columns
         value_phenotypes = self._get_value_characteristics()
         value_columns = [f"{x.name}_VALUE" for x in value_phenotypes]
-        logger.debug(f'Found {len(value_columns)} : {value_columns}')
-        
+        logger.debug(f"Found {len(value_columns)} : {value_columns}")
+
         if len(value_columns) == 0:
             return None
 
