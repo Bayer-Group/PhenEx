225e0794eb245182018ab9bbab6165f85dc5e34e
a-hartens <alexander.hartenstein@bayer.com>
2025-05-14 18:34:20 +0200

    added documentation

--- phenex/phenotypes/within_same_encounter_phenotype.py
+++ phenex/phenotypes/within_same_encounter_phenotype.py
@@ -3,6 +3,54 @@ from phenex.phenotypes.phenotype import Phenotype
 
 
 class WithinSameEncounterPhenotype(Phenotype):
+    """
+    WithinSameEncounterPhenotype is a phenotype that filters a target phenotype based on the occurrence of an anchor phenotype within the same encounter. This phenotype can only be used with CodelistPhenotypes and MeasurementPhenotypes as anchor/phenotype.
+
+    Parameters:
+        name: The name of the phenotype. Optional. If not passed, name will be derived from the name of the codelist.
+        anchor_phenotype: The phenotype which should define the encounter of interest
+        phenotype: The phenotype which should occur within the same encounter of interest, defined by the anchor_phenotype
+        column_name: The name of the column which must be shared between anchor_phenotype and phenotype
+
+
+    Attributes:
+        table (PhenotypeTable): The resulting phenotype table after filtering (None until execute is called)
+
+    Examples:
+
+    Example: Imaging procedure within the same encounter as Pulmonary Embolism (OMOP)
+        ```python
+        from phenex.phenotypes import CodelistPhenotype, WithinSameEncounterPhenotype
+        from phenex.codelists import Codelist
+        from phenex.mappers import OMOPDomains
+        from phenex.filters import DateFilter, CategoricalFilter, Value
+        from phenex.ibis_connect import SnowflakeConnector
+
+        pe_diagnosis = CodelistPhenotype(
+            name = "diagnosis_of_PE",
+            domain = "CONDITION_OCCURRENCE",
+            codelist = Codelist(codelist={'SNOMED': [608954, 4120091, 45768439, 45768888, 37165995, 436768, 44782732, 45768887, 45771016, 4017134, 4240832, 4129841, 43530934, 4219469, 442055, 442051, 442050, 433545, 437060, 435026, 433832, 440477, 439380, 439379, 4331168, 4108681, 37160752, 4091708, 1244882, 440417, 37109911, 3655209, 3655210, 45757145, 37016922, 43530605, 4119608, 4253796, 45766471, 4121618, 4236271, 36713113, 35615055, 40479606, 4119607, 4119609]}).copy(use_code_type=False),
+            return_date = "first",
+        )
+
+        # where column id alone is idnetical left join
+        imaging = CodelistPhenotype(
+            name = 'imaging',
+            domain = 'PROCEDURE_OCCURRENCE',
+            codelist = Codelist(codelist={'CPT4': [2211381, 2211379, 2211380, 2212002, 2212004, 2212003, 2212011, 42742555, 42742554, 2212006, 2212005, 2212008, 2212010, 2212009, 42742557, 42742556]}).copy(use_code_type=False),
+        )
+
+
+        imaging_in_entry_hospitalization = WithinSameEncounterPhenotype(
+            name='imaging_in_entry_hospitalization',
+            anchor_phenotype = pe_diagnosis,
+            phenotype = imaging,
+            column_name = 'VISIT_OCCURRENCE_ID'
+        )
+
+        ```
+    """
+
     def __init__(
         self,
         name: str,
