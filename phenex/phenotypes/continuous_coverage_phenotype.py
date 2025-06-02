02c7f50c8846f46eb18c79a7f54092f0b32f792b
Stephen Privitera <stephen.privitera@bayer.com>
2025-06-02 16:44:06 +0200

    value filter for ccpt

--- phenex/phenotypes/continuous_coverage_phenotype.py
+++ phenex/phenotypes/continuous_coverage_phenotype.py
@@ -1,15 +1,11 @@
 from typing import Union, List, Dict, Optional
-from phenex.tables import PhenexObservationPeriodTable
 from phenex.phenotypes.phenotype import Phenotype
-from phenex.filters.value import GreaterThanOrEqualTo, Value
-from phenex.filters.codelist_filter import CodelistFilter
+from phenex.filters.value import GreaterThanOrEqualTo
 from phenex.filters.relative_time_range_filter import (
     verify_relative_time_range_filter_input,
     RelativeTimeRangeFilter,
 )
-from phenex.filters.date_filter import DateFilter
-from phenex.aggregators import First, Last
-from phenex.codelists import Codelist
+from phenex.filters.date_filter import ValueFilter
 from phenex.tables import is_phenex_code_table, PHENOTYPE_TABLE_COLUMNS, PhenotypeTable
 from phenex.phenotypes.functions import select_phenotype_columns
 from ibis import _
@@ -35,52 +31,52 @@ class ContinuousCoveragePhenotype(Phenotype):
 
     One assumption that is made by ContinuousCoveragePhenotype is that there are **NO overlapping coverage periods**.
 
+    Parameters:
+        name: The name of the phenotype.
+        domain: The domain of the phenotype. Default is 'observation_period'.
+        value_filter: Fitler returned persons based on the duration of coverage in days.
+        anchor_phenotype: An anchor phenotype defines the reference date with respect to calculate coverage. In typical applications, the anchor phenotype will be the entry criterion.
+        when: 'before', 'after'. If before, the return date is the start of the coverage period containing the anchor_phenotype. If after, the return date is the end of the coverage period containing the anchor_phenotype.
+
+    Example:
+    ```python
+    # make sure to create an entry phenotype, for example 'atrial fibrillation diagnosis'
+    entry_phenotype = CodelistPhenotype(...)
+    # one year continuous coverage prior to index
+    one_year_coverage = ContinuousCoveragePhenotype(
+        when = 'before',
+        value_filter = ValueFilter(
+            min_value=GreaterThanOrEqualTo(365)
+            ),
+        anchor_phenotype = entry_phenotype
+    )
+    # determine the date of loss to followup
+    loss_to_followup = ContinuousCoveragePhenotype(
+        when = 'after',
+        anchor_phenotype = entry_phenotype
+    )
+    ```
     """
 
     def __init__(
         self,
         name: Optional[str] = "continuous_coverage",
         domain: Optional[str] = "OBSERVATION_PERIOD",
-        min_days: Optional[Value] = None,
-        max_days: Optional[Value] = None,
+        value_filter: Optional[ValueFilter] = None,
         when: Optional[str] = "before",
         anchor_phenotype: Optional[Phenotype] = None,
         **kwargs
     ):
-        """
-        Parameters:
-            name: The name of the phenotype. Default is provided by parameters when, min_days, max_days, and anchor_phenotype.
-            domain: The domain of the phenotype. Default is 'observation_period'.
-            min_days: The minimum number of days of continuous coverage. The operator must be '>=' or '>'.
-            max_days: The maximum number of days of continuous coverage. The operator must be '<=' or '<'.
-            when: 'before', 'after'. If before, the return date is the start of the coverage period containing the anchor_phenotype. If after, the return date is the end of the coverage period containing the anchor_phenotype.
-        Example :
-        ```python
-
-        # make sure to create an entry phenotype, for example 'atrial fibrillation diagnosis'
-        entry_phenotype = CodelistPhenotype(...)
-
-        # one year continuous coverage prior to index
-        one_year_coverage = ContinuousCoveragePhenotype(
-            when = 'before',
-            min_days = GreaterThanOrEqualTo(365),
-            anchor_phenotype = entry_phenotype
-        )
-
-        # determine the date of loss to followup
-        loss_to_followup = ContinuousCoveragePhenotype(
-            when = 'after',
-            anchor_phenotype = entry_phenotype
-        )
-        ```
-        """
         super().__init__(**kwargs)
         self.name = name
         self.domain = domain
-        verify_relative_time_range_filter_input(min_days, max_days, when)
-        self.min_days = min_days
-        self.max_days = max_days
         self.when = when
+        self.min_days = self.max_days = None
+        if value_filter:
+            self.min_days = value_filter.min_value
+            self.max_days = value_filter.max_value
+
+        verify_relative_time_range_filter_input(self.min_days, self.max_days, self.when)
         self.anchor_phenotype = anchor_phenotype
         if self.anchor_phenotype is not None:
             self.children.append(self.anchor_phenotype)
