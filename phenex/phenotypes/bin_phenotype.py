be115a6203778de51398756b08060706f3dfe759
a-hartens <alexander.hartenstein@bayer.com>
2025-07-31 16:25:38 +0200

    black

--- phenex/phenotypes/bin_phenotype.py
+++ phenex/phenotypes/bin_phenotype.py
@@ -15,8 +15,8 @@ logger = create_logger(__name__)
 class BinPhenotype(Phenotype):
     """
     BinPhenotype converts numeric values into categorical bin labels.
-    
-    Takes a phenotype that returns numeric values (like age, measurements, etc.) 
+
+    Takes a phenotype that returns numeric values (like age, measurements, etc.)
     and converts the VALUE column into bin labels like "[10-20)", "[20-30)", etc.
 
     DATE: The event date selected from the input phenotype
@@ -41,7 +41,7 @@ class BinPhenotype(Phenotype):
 
         tables = {"PERSON": example_person_table}
         result_table = binned_age.execute(tables)
-        
+
         # Result will have VALUE column with labels like "[20-30)", "[30-40)", etc.
         display(result_table)
         ```
@@ -50,56 +50,63 @@ class BinPhenotype(Phenotype):
     def __init__(
         self,
         phenotype: Phenotype,
-        bins = list(range(0, 101, 10)),
+        bins=list(range(0, 101, 10)),
         **kwargs,
     ):
         super(BinPhenotype, self).__init__(**kwargs)
         self.bins = bins
         self.phenotype = phenotype
-        if self.phenotype.__class__.__name__ not in ["AgePhenotype", "MeasurementPhenotype", "ArithmeticPhenotype", "ScorePhenotype"]:
-            raise ValueError(f"Invalid phenotype type: {self.phenotype.__class__.__name__}")
+        if self.phenotype.__class__.__name__ not in [
+            "AgePhenotype",
+            "MeasurementPhenotype",
+            "ArithmeticPhenotype",
+            "ScorePhenotype",
+        ]:
+            raise ValueError(
+                f"Invalid phenotype type: {self.phenotype.__class__.__name__}"
+            )
         self.children = [phenotype]
 
     def _execute(self, tables) -> PhenotypeTable:
         # Execute the child phenotype to get the initial table to filter
         table = self.phenotype.table
-                
+
         # Create bin labels
         bin_labels = []
-        
+
         # Add a bin for values < first bin edge
         bin_labels.append(f"<{self.bins[0]}")
-        
+
         # Add bins for each range
         for i in range(len(self.bins) - 1):
             bin_labels.append(f"[{self.bins[i]}-{self.bins[i+1]})")
-        
+
         # Add a final bin for values >= last bin edge
         bin_labels.append(f">={self.bins[-1]}")
-        
+
         # Create binning logic using Ibis case statements
         value_col = table.VALUE
-        
+
         # Start with the case expression
         case_expr = None
-        
+
         # Handle values < first bin edge
         first_condition = value_col < self.bins[0]
         case_expr = ibis.case().when(first_condition, bin_labels[0])
-        
+
         # Create conditions for each bin range
         for i in range(len(self.bins) - 1):
             condition = (value_col >= self.bins[i]) & (value_col < self.bins[i + 1])
             case_expr = case_expr.when(condition, bin_labels[i + 1])
-        
+
         # Handle values >= last bin edge
         final_condition = value_col >= self.bins[-1]
         case_expr = case_expr.when(final_condition, bin_labels[-1])
-        
+
         # Handle null values
         case_expr = case_expr.else_(None)
-        
+
         # Replace the VALUE column with bin labels
         table = table.mutate(VALUE=case_expr.end())
-        
+
         return table
--- phenex/test/phenotypes/test_bin_phenotype.py
+++ phenex/test/phenotypes/test_bin_phenotype.py
@@ -1,4 +1,3 @@
-
 import datetime, os
 import pandas as pd
 
@@ -15,17 +14,19 @@ from phenex.filters import Value, ValueFilter
 
 from phenex.mappers import *
 
+
 class BinnedAgePhenotypeTestGenerator(PhenotypeTestGenerator):
     name_space = "bnpt_age"
     value_datatype = str
     test_values = True
+
     def define_input_tables(self):
         index_date = datetime.date(2022, 1, 1)
 
-        self.n_persons = int(20/5) + 1
+        self.n_persons = int(20 / 5) + 1
 
         birth_dates = [
-            index_date - datetime.timedelta(days=365.25 * i*5)
+            index_date - datetime.timedelta(days=365.25 * i * 5)
             for i in range(self.n_persons)
         ]
         N = len(birth_dates)
@@ -42,52 +43,36 @@ class BinnedAgePhenotypeTestGenerator(PhenotypeTestGenerator):
 
         return [input_info_person]
 
-    
     def define_phenotype_tests(self):
         agept = AgePhenotype()
         ages_matching = list(range(self.n_persons))
         t1 = {
             "name": "bins",
             "persons": [f"P{x}" for x in ages_matching],
-            "values": [
-                "[0-10)",
-                "[0-10)",
-                "[10-20)",
-                "[10-20)",
-                "[20-30)"
-            ],
+            "values": ["[0-10)", "[0-10)", "[10-20)", "[10-20)", "[20-30)"],
             "phenotype": BinPhenotype(
                 phenotype=agept,
-            )
+            ),
         }
 
         t2 = {
             "name": "bins_out_of_range",
             "persons": [f"P{x}" for x in ages_matching],
-            "values": [
-                "<10",
-                "<10",
-                "[10-20)",
-                "[10-20)",
-                ">=20"
-            ],
-            "phenotype": BinPhenotype(
-                phenotype=agept,
-                bins = [10, 20]
-            )
+            "values": ["<10", "<10", "[10-20)", "[10-20)", ">=20"],
+            "phenotype": BinPhenotype(phenotype=agept, bins=[10, 20]),
         }
-        test_infos = [
-            t1, t2
-        ]
-       
+        test_infos = [t1, t2]
+
         for test_info in test_infos:
-            test_info["phenotype"].name = test_info['name']
+            test_info["phenotype"].name = test_info["name"]
 
         return test_infos
+
 
 def test_binned_age_phenotype():
     spg = BinnedAgePhenotypeTestGenerator()
     spg.run_tests()
+
 
 if __name__ == "__main__":
     test_binned_age_phenotype()
