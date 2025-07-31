7725e76d05981d43662d7a706b23cb1fba81ddc9
a-hartens <alexander.hartenstein@bayer.com>
2025-07-31 16:22:18 +0200

    functional tests for bin phenotype

--- phenex/test/phenotypes/test_bin_phenotype.py
+++ phenex/test/phenotypes/test_bin_phenotype.py
@@ -15,24 +15,69 @@ from phenex.filters import Value, ValueFilter
 
 from phenex.mappers import *
 
-from .test_age_phenotype import AgePhenotypeTestGenerator
-
-class BinnedAgePhenotypeTestGenerator(AgePhenotypeTestGenerator):
+class BinnedAgePhenotypeTestGenerator(PhenotypeTestGenerator):
     name_space = "bnpt_age"
+    value_datatype = str
+    test_values = True
+    def define_input_tables(self):
+        index_date = datetime.date(2022, 1, 1)
 
+        self.n_persons = int(20/5) + 1
+
+        birth_dates = [
+            index_date - datetime.timedelta(days=365.25 * i*5)
+            for i in range(self.n_persons)
+        ]
+        N = len(birth_dates)
+
+        df_person = pd.DataFrame()
+        df_person["PERSON_ID"] = [f"P{x}" for x in list(range(N))]
+        df_person["DATE_OF_BIRTH"] = birth_dates
+        df_person["INDEX_DATE"] = index_date
+
+        input_info_person = {
+            "name": "PERSON",
+            "df": df_person,
+        }
+
+        return [input_info_person]
+
+    
     def define_phenotype_tests(self):
         agept = AgePhenotype()
         ages_matching = list(range(self.n_persons))
         t1 = {
             "name": "bins",
             "persons": [f"P{x}" for x in ages_matching],
-            "values": ages_matching,
+            "values": [
+                "[0-10)",
+                "[0-10)",
+                "[10-20)",
+                "[10-20)",
+                "[20-30)"
+            ],
             "phenotype": BinPhenotype(
                 phenotype=agept,
             )
         }
+
+        t2 = {
+            "name": "bins_out_of_range",
+            "persons": [f"P{x}" for x in ages_matching],
+            "values": [
+                "<10",
+                "<10",
+                "[10-20)",
+                "[10-20)",
+                ">=20"
+            ],
+            "phenotype": BinPhenotype(
+                phenotype=agept,
+                bins = [10, 20]
+            )
+        }
         test_infos = [
-            t1
+            t1, t2
         ]
        
         for test_info in test_infos:
