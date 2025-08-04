f11bfa16178e46a1271f1d4cbae8f5a4107e4793
a-hartens <alexander.hartenstein@bayer.com>
2025-08-04 08:22:23 +0200

    updates to splash page

--- app/ui/src/components/MainView/SplashPage/SplashPage.tsx
+++ app/ui/src/components/MainView/SplashPage/SplashPage.tsx
@@ -10,24 +10,18 @@ export const SplashPage = () => {
         <div className={styles.header}>
           <img src={phenexLogo} alt="PhenEx Logo" className={styles.logo} />
 
-          <h1>Welcome to PhenEx</h1>
         </div>
-        <p>
-          PhenEx allows you to define the medical content of an observational study using Real World
-          Data.
-        </p>
-        <p>
-          Which patients are you interested in observing? How do you want to characterize them? What
-          outcomes are you interested in observing?
-        </p>
-        <p>
-          Once defined, PhenEx automatically creates your analysis dataset with tested, reliable
-          code.
-        </p>
-        <p>
-          To get started, create a cohort or select an existing one in the left navigation area, or
-          ask AI to generate one for you.
-        </p>
+        <div className={styles.text}>
+          <p>
+            <span className={styles.title}>PhenEx</span> allows you to define the medical content of an observation study in real world data.
+          </p>
+          <p>
+            The code to extract this data is then <emph>automatically generated</emph>, giving you your analysis ready cohort, table 1, and basic outcome reporting.
+          </p>
+          <p>
+            To get started, <emph>create a cohort</emph> or select an existing one in the left navigation area, or ask AI to generate one for you.
+          </p>
+      </div>
       </div>
       <div className={`${styles.label} ${styles.bottomRight}`}></div>
     </div>
