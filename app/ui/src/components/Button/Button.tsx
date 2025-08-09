174480ac24c75589e673395ef1e2b7cc53d064ac
a-hartens <alexander.hartenstein@bayer.com>
2025-08-09 16:51:27 +0200

    adding missing popover refactor files

---
+++ app/ui/src/components/Button/Button.tsx
@@ -0,0 +1,26 @@
+import { FC } from 'react';
+import styles from './Button.module.css';
+
+interface ButtonProps {
+  onClick: () => void;
+  title?: string;  // for accessibility and tooltips
+  className?: string;
+}
+
+export const Button: FC<ButtonProps> = ({ 
+  onClick, 
+  title = 'Close', 
+  className = '' 
+}) => {
+
+  return (
+    <button
+      className={`${styles.button} ${className}`}
+      onClick={onClick}
+      title={title}
+      aria-label={title}
+    >
+      {title}
+    </button>
+  );
+};
---
+++ app/ui/src/components/PopoverHeader/PopoverHeader.tsx
@@ -0,0 +1,34 @@
+import { FC } from 'react';
+import styles from './PopoverHeader.module.css';
+import {XButton} from '../XButton/XButton';
+
+interface PopoverHeader {
+  onClick: () => void;
+  title?: string;  // for accessibility and tooltips
+  className?: string;
+  children?: React.ReactNode;
+}
+
+export const PopoverHeader: FC<PopoverHeader> = ({ 
+  onClick, 
+  title = 'Close', 
+  className = '',
+  children = undefined
+}) => {
+
+  return (
+    <div
+      className={`${styles.popoverheader} ${className}`}
+      onClick={onClick}
+      title={title}
+      aria-label={title}
+    >
+      {!children && title}
+      {children && <div className={styles.content}>{children}</div>}
+      <XButton
+        onClick={undefined}
+        className = {styles.xButton}
+      />
+    </div>
+  );
+};
