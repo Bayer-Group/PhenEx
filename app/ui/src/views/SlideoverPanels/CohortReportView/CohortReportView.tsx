dd20c905f226765d372440e996ee209c43a38aca
a-hartens <alexander.hartenstein@bayer.com>
2025-08-18 15:21:44 +0200

    added info to report tab

--- app/ui/src/views/SlideoverPanels/CohortReportView/CohortReportView.tsx
+++ app/ui/src/views/SlideoverPanels/CohortReportView/CohortReportView.tsx
@@ -55,8 +55,33 @@ export const CohortReportView: FC<CohortReportViewProps> = ({ data }) => {
     }
   };
 
+  const infoContent = () => {
+    return (
+      <span>
+        <i>View results of cohort execution</i>
+        <ul>
+          <li>
+            <em>Attrition diagrams</em> : Click the attrition tab to see how your entry, inclusion and exclusion criteria interact to build your final cohort. Phenotypes are run in order they are displayed in the cohort phenotype editor. 
+            <ul>
+              <li>The N column displays how many patients in the cohort meet each criterion</li>
+              <li>The Waterfall column displays how many patients remain after executing the phenotype on that row in order</li>
+              <li>The delta column displays how many patients were lost after applying the phenotype in that row in order</li>
+            </ul>
+          </li>
+          <li>
+            <em>Table 1</em> displays your baseline characteristics. The N column displays how many patients in the cohort fulfill the criteria in the row. Summary statistics are shown for numerically valued phenotypes. Categorical phenotypes are split into separate rows.
+          </li>
+          <li>
+            <em>Kaplan Meier plots</em> are provided for all outcomes.
+          </li>
+        </ul>
+      </span>
+    );
+  };
+
+
   return (
-    <SlideoverPanel title="Report">
+    <SlideoverPanel title="Report" info={infoContent()}>
       <div className={styles.container}>
         {/* <div className={styles.topSection}>
           <div className={styles.controlsContainer}>
