3ec6d783a830105abdd5aa9bd40bd35b7a093eea
a-hartens <alexander.hartenstein@bayer.com>
2025-08-19 17:56:07 +0200

    added text area to infopanel

--- app/ui/src/views/SlideoverPanels/InfoPanel/InfoPanel.tsx
+++ app/ui/src/views/SlideoverPanels/InfoPanel/InfoPanel.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import styles from './InfoPanel.module.css';
 import { SlideoverPanel } from '../SlideoverPanel/SlideoverPanel';
+import { CohortTextArea } from '../../CohortViewer/CohortInfo/CohortTextArea/CohortTextArea';
 
 export const InfoPanel: React.FC = () => {
   const infoContent = () => {
     return (
       <span>
-        <i>Information panel for additional details and help</i>
+        <i>Information panel with rich text editor</i>
         <ul>
           <li>
-            This panel can be used to display <em>contextual information</em> and help content
+            Use this panel to <em>create and edit notes</em> with rich text formatting
           </li>
           <li>
-            Content can be customized based on the current context or user selection
+            The editor supports <em>bold, italic, lists, headers</em> and more formatting options
+          </li>
+          <li>
+            Perfect for <em>documentation, notes, or detailed information</em> that needs formatting
           </li>
         </ul>
       </span>
@@ -22,9 +26,9 @@ export const InfoPanel: React.FC = () => {
   return (
     <SlideoverPanel title="Info" info={infoContent()}>
       <div className={styles.container}>
-        <div>
-          <p>This is a simple InfoPanel component.</p>
-          <p>You can add any content here that provides information or help to users.</p>
+        <div className={styles.editorWrapper}>
+          <h3>Notes & Information</h3>
+          <CohortTextArea />
         </div>
       </div>
     </SlideoverPanel>
