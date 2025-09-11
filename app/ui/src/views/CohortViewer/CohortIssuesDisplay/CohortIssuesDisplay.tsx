a64cc9bf356c92cfc79a489e95afae1814d11e8a
a-hartens <alexander.hartenstein@bayer.com>
2025-09-11 13:21:52 +0200

    adding icon to issues display

--- app/ui/src/views/CohortViewer/CohortIssuesDisplay/CohortIssuesDisplay.tsx
+++ app/ui/src/views/CohortViewer/CohortIssuesDisplay/CohortIssuesDisplay.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import styles from './CohortIssuesDisplay.module.css';
 import { XButton } from '../../../components/ButtonsAndTabs/XButton/XButton';
+import BirdIcon from '../../../assets/bird_icon.png'
 
 export interface CohortIssue {
   phenotype_id: string;
@@ -41,19 +42,26 @@ export const CohortIssuesDisplay: React.FC<CohortIssuesDisplayProps> = ({
     return renderUnselectedTitleLabel();
   };
 
+  const renderBird = () => {
+    return (
+      <img src={BirdIcon} alt="No issues" className={`${styles.birdIcon} ${hasIssues ? styles.birdIssues : styles.birdNoIssues}`} />
+    );
+  }
+
   return (
     <div
       className={`${styles.row} ${selected ? styles.selected : ''} ${hasIssues ? styles.hasIssues : styles.noIssues}`}
       onClick={selected ? onClick : undefined}
     >
       <span className={styles.text}>
-        {hasIssues ? renderText() : <span className={styles.labelNoIssues}></span>}
+        {hasIssues ? renderText() : ''}
       </span>
       <div
         className={`${styles.statusDot} ${hasIssues ? styles.red : styles.green} ${selected ? styles.selected : ''}`}
       >
         {totalIssueCount}
       </div>
+      {renderBird()}
       {selected && renderXButton()}
     </div>
   );
