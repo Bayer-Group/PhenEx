63c3e613a38e78c0b9c15655a5051d7251e55b58
a-hartens <alexander.hartenstein@bayer.com>
2025-09-24 11:03:01 +0200

    first try to add codelists database

--- app/backend/migrate/100_initial.up.sql
+++ app/backend/migrate/100_initial.up.sql
@@ -22,4 +22,18 @@ CREATE TABLE IF NOT EXISTS "cohort" (
     FOREIGN KEY("user_id") REFERENCES "user" ("id")
 );
 
+CREATE TABLE IF NOT EXISTS "codelistfile" (
+    "codelist_id" character varying NOT NULL,
+    "user_id" uuid NOT NULL,
+    "version" integer NOT NULL DEFAULT 1,
+    "codelist_data" jsonb NOT NULL,
+    "column_mapping" jsonb NOT NULL, -- JSON object mapping column names to the default column names used by PhenEx
+    "codelists" character varying[] NOT NULL,  -- Array of strings which lists codelist names contained in the file
+    "created_at" timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY ("codelist_id"),
+    FOREIGN KEY("user_id") REFERENCES "user" ("id")
+    FOREIGN KEY("cohort_id") REFERENCES "cohort" ("cohort_id")
+);
+
 COMMIT;
