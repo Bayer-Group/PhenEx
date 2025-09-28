2be5ef613f95d55727aeb2c77d490e0855c3fe3c
a-hartens <alexander.hartenstein@bayer.com>
2025-09-28 11:22:13 +0200

    added study to database

--- app/backend/migrate/100_initial.up.sql
+++ app/backend/migrate/100_initial.up.sql
@@ -9,9 +9,27 @@ CREATE TABLE "user" (
     PRIMARY KEY ("id")
 );
 
+CREATE TABLE IF NOT EXISTS "study" (
+    "study_id" character varying NOT NULL,
+    "user_id" uuid NOT NULL, -- created_by user
+    "visible_by" uuid[] DEFAULT '{}', -- array of user_ids who can see this study
+    "is_public" boolean DEFAULT FALSE,
+    "name" character varying NOT NULL,
+    "description" text,
+    "baseline_characteristics" jsonb,
+    "outcomes" jsonb,
+    "analysis" jsonb,
+    "created_at" timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY ("study_id"),
+    FOREIGN KEY("user_id") REFERENCES "user" ("id")
+);
+
 CREATE TABLE IF NOT EXISTS "cohort" (
     "cohort_id" character varying NOT NULL,
     "user_id" uuid NOT NULL,
+    "study_id" character varying NOT NULL, -- many cohorts to one study
+    "parent_cohort_id" character varying NULL, -- for subcohorts, references parent cohort
     "version" integer NOT NULL DEFAULT 1,
     "cohort_data" jsonb NOT NULL,
     "is_provisional" boolean DEFAULT FALSE,
@@ -19,7 +37,9 @@ CREATE TABLE IF NOT EXISTS "cohort" (
     "created_at" timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
     "updated_at" timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY ("cohort_id"),
-    FOREIGN KEY("user_id") REFERENCES "user" ("id")
+    FOREIGN KEY("user_id") REFERENCES "user" ("id"),
+    FOREIGN KEY("study_id") REFERENCES "study" ("study_id"),
+    FOREIGN KEY("parent_cohort_id") REFERENCES "cohort" ("cohort_id")
 );
 
 CREATE TABLE IF NOT EXISTS "codelistfile" (
