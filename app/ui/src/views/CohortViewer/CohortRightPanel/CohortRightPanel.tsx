9e34806cb3eff3a6a1895d00045131cda6366962
a-hartens <alexander.hartenstein@bayer.com>
2026-01-30 19:01:21 +0100

    improved scrolling

--- app/ui/src/views/CohortViewer/CohortRightPanel/CohortRightPanel.tsx
+++ app/ui/src/views/CohortViewer/CohortRightPanel/CohortRightPanel.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { InfoPanel } from '../../SlideoverPanels/InfoPanel/InfoPanel';
 import { DatabasePanel } from '../../SlideoverPanels/DatabasePanel/DatabasePanel';
 import { CodelistsViewer } from '../../SlideoverPanels/CodelistsViewer/CodelistsViewer';
@@ -6,6 +6,7 @@ import { ConstantsPanel } from '../../SlideoverPanels/ConstantsPanel/ConstantsPa
 import { VisibilityPanel } from '../../SlideoverPanels/VisibilityPanel/VisibilityPanel';
 import { CohortReportView } from '../../SlideoverPanels/CohortReportView/CohortReportView';
 import { ExecutePanel } from '../../SlideoverPanels/ExecutePanel/ExecutePanel';
+import { SimpleCustomScrollbar } from '../../../components/CustomScrollbar/SimpleCustomScrollbar';
 import styles from './CohortRightPanel.module.css';
 
 interface CohortRightPanelProps {
@@ -13,14 +14,21 @@ interface CohortRightPanelProps {
 }
 
 export const CohortRightPanel: React.FC<CohortRightPanelProps> = ({ contentMode = 'cohort' }) => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  
   return (
-    <div className={styles.container}>
-      <InfoPanel />
-      <DatabasePanel />
-      <CodelistsViewer />
-      <ConstantsPanel />
-      <VisibilityPanel />
-      <ExecutePanel />
+    <div className={styles.wrapper}>
+      <div ref={containerRef} className={styles.container}>
+        <InfoPanel />
+        <DatabasePanel />
+        <CodelistsViewer />
+        <ConstantsPanel />
+        <VisibilityPanel />
+        <ExecutePanel />
+      </div>
+      <SimpleCustomScrollbar targetRef={containerRef} 
+        marginToEnd={10}
+      />
     </div>
   );
 };
