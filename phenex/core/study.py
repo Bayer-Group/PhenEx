c5d486ca1c85ccd94fb600b5aaa8e52fe0a789e1
a-hartens <alexander.hartenstein@bayer.com>
2026-02-07 23:28:42 +0100

    adding study class. incomplete

---
+++ phenex/core/study.py
@@ -0,0 +1,103 @@
+
+import os, datetime, json
+from typing import List, Dict, Optional
+from phenex.node import Node, NodeGroup
+import ibis
+from phenex.util.serialization.to_dict import to_dict
+from phenex.util import create_logger
+from phenex.core.cohort import Cohort
+
+logger = create_logger(__name__)
+
+
+class Study:
+    """
+    The Study
+
+    Parameters:
+
+
+    Attributes:
+
+    """
+
+    def __init__(self, path:str, name: str, cohorts: List[Cohort], custom_reporters: List["Reporter"]):
+        self.path = path
+        self.name = name
+        self.cohorts = cohorts
+        self.custom_reporters = reporters
+
+        self._create_study_output_path()
+        self._check_cohort_names_unique()
+        self._check_cohorts_have_databases()
+
+    def _create_study_output_path(self):
+        # ensure that the output path directory is the name of the study
+        if self.path.split(os.sep)[-1] != self.name:
+            self.path = os.path.join(self.path, self.name)
+        # ensure directory exists
+        if not os.path.exists(self.path):
+            os.makedirs(self.path)
+
+    def _check_cohort_names_unique(self):
+        all_names = [x.name for x in self.cohorts]
+        unique_names = list(set(all_names))
+        if len(all_names) != len(unique_names):
+            raise ValueError(f"Ensure that cohort names are unique; found cohort names {sorted(all_names)}")
+    
+    def _check_cohorts_have_databases(self):
+        missing_database = []
+        for cohort in self.cohorts:
+            if cohort.database is None:
+                missing_database.append(cohort)
+        if len(missing_database)>0:
+            raise ValueError(f"Cohorts must have databases defined in order for use in a Study. Cohorts missing database : {[x.name for x in missing_database]}")
+            
+    
+    def execute(
+        self,
+        overwrite: Optional[bool] = False,
+        n_threads: Optional[int] = 1,
+        lazy_execution: Optional[bool] = False,
+    ):
+        path_exec_dir_study = self._prepare_study_execution_directory()
+        self._freeze_software_versions(path_exec_dir_study)
+        for cohort in self.cohorts:
+            path_exec_dir_cohort = self._prepare_cohort_execution_directory(cohort, path_exec_dir_study)
+            self._save_serialized_cohort(cohort, path_exec_dir_cohort)
+
+            # cohort.execute(
+            #     overwrite=overwrite, lazy_execution=lazy_execution, n_threads=n_threads
+            # )
+
+            for reporter in self.reporters:
+                reporter.execute(cohort)
+                reporter
+
+    def _prepare_study_execution_directory(self):
+        now = datetime.datetime.today()
+        # TODO single digit integers should have a 0 in front (currently just single digit)
+        dirname = f"{now.year}-{now.month}-{now.day}__{now.hour}-{now.minute}"
+        path = os.path.join(self.path, dirname)
+        print(path)
+        if os.path.exists(path):
+            logger.warning(f"Output directory {path} already exists!")
+        else:
+            logger.info(f"Creating output directory for study execution : {path}")
+            os.makedirs(path)
+        return path
+
+    def _freeze_software_versions(self):
+        # store python and phenex versions in .txt file 
+        pass
+
+    def _prepare_cohort_execution_directory(self, cohort, path_exec_dir_study):
+        _path = os.path.join(path_exec_dir_study, cohort.name)
+        if not os.path.exists(_path):
+            os.makedirs(_path)
+        return _path
+
+    def _save_serialized_cohort(self, cohort, path_exec_dir_cohort):
+        _path = os.path.join(path_exec_dir_cohort, cohort.name+'.json')
+        with open(_path, 'w') as f:
+            json.dump(cohort.to_dict(),f)
