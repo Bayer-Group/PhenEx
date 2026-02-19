e753417a63d187e86636a6d1e6649f0a35c7949a
a-hartens <alexander.hartenstein@bayer.com>
2026-02-19 13:06:32 -0700

    removing waterful colorful

--- phenex/reporting/__init__.py
+++ phenex/reporting/__init__.py
@@ -3,7 +3,6 @@ from .table1 import Table1
 from .table2 import Table2
 from .counts import InExCounts
 from .waterfall import Waterfall
-from .waterfall_colorful import WaterfallColorful
 from .time_to_event import TimeToEvent
 from .cohort_explorer import CohortExplorer
 from .report_drafter import ReportDrafter
