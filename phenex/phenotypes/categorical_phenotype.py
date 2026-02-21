53ef1ad2f628ddd4eca28cb1865b3c6ba5408801
a-hartens <alexander.hartenstein@bayer.com>
2026-02-21 06:53:36 -0700

    adding docstring

--- phenex/phenotypes/categorical_phenotype.py
+++ phenex/phenotypes/categorical_phenotype.py
@@ -10,7 +10,9 @@ from phenex.tables import PHENOTYPE_TABLE_COLUMNS, PhenotypeTable
 from phenex.phenotypes.functions import select_phenotype_columns
 from ibis import _
 import ibis
+from phenex.util import create_logger
 
+logger = create_logger(__name__)
 
 def check_categorical_filters_share_same_domain(filter, domain):
     # if any leaf node of the tree has a different domain, return false
@@ -32,18 +34,65 @@ def check_categorical_filters_share_same_domain(filter, domain):
 
 class CategoricalPhenotype(Phenotype):
     """
-    CategoricalPhenotype calculates phenotype whose VALUE is discrete, such for sex, race, or ethnicity. Categorical Phenotype is especially helpful as a baseline characteristic from PERSON like tables.
-    The returned Phenotype has the following interpretation:
-
-    DATE: If when='before', then DATE is the beginning of the coverage period containing the anchor phenotype. If when='after', then DATE is the end of the coverage period containing the anchor date.
-    VALUE: Coverage (in days) relative to the anchor date. By convention, always non-negative.
+    CategoricalPhenotype is used for discrete entities such for sex, race, or ethnicity, diagnosis position, or encounter type. CategoricalPhenotypes are especially helpful as a baseline characteristic from PERSON like tables to identify demographic information.
+    
+    CategoricalPhenotype can be used to filter patients by a category, or to pull relevant categorical information. 
+    
+    
+    DATE: Often null; only populated if the categorical value is associated with a date e.g.a categorical phenotype identifying all inpatient encounters in an event table
+    VALUE: The identified category from the source column.
 
 
     Parameters:
         name: Name of the phenotype.
         domain: Domain of the phenotype.
         categorical_filter: Use CategoricalFilter to input allowed values for the categorical variable. If not passed, all values are returned.
+    
+    Example: Get female patients where categorical variable is in the PERSON table
+        ```python
+        # create a filter defining the allowed values for the categorical variable
+        f_female = CategoricalFilter(
+            column_name='GENDERSOURCEVALUE',
+            allowed_values=['F'],
+        )
+
+        # create a categorical phenotype using the filter
+        pt_female = CategoricalPhenotype(
+            name="female_patients",
+            domain='PERSON',
+            categorical_filter=f_female,
+        )
+        ```
+        
+    Example: Get categorical variable assigned to patients without performing filtering
+        ```python
+        f_any_sex = CategoricalFilter(
+            column_name='GENDERSOURCEVALUE',
+            operator='notnull',
+        )
+        pt_any_sex = CategoricalPhenotype(
+            name="sex_of_patients",
+            domain='PERSON',
+            categorical_filter=f_any_sex,
+        )
+        ```
+
+        Example: Get categorical variable assigned to patients where concept tables are used e.g. a race table with ids but actual value exists on a CONCEPT table.
+        ```python
+        # define the nonnull filter on the CONCEPT table
+        f_race_concept_name = CategoricalFilter(
+            column_name='CONCEPT_NAME',
+            operator='notnull',
+            domain='CONCEPT'
+        )
+        pt_race = CategoricalPhenotype(
+            name="race",
+            domain='RACETABLE',
+            categorical_filter=f_race_concept_name,
+        )
+        ```
     """
+
 
     output_display_type = "categorical"
 
@@ -61,11 +110,11 @@ class CategoricalPhenotype(Phenotype):
     ):
         super(CategoricalPhenotype, self).__init__(name=name, **kwargs)
         self.domain = domain
-        # if categorical_filter is not None:
-        #     if not check_categorical_filters_share_same_domain(
-        #         categorical_filter, self.domain
-        #     ):
-        #         logger.info("CategoricalPhenotype only works on a single domain.")
+        if categorical_filter is not None:
+            if not check_categorical_filters_share_same_domain(
+                categorical_filter, self.domain
+            ):
+                logger.info(f"CategoricalPhenotype {self.name} operates on multiple tables {self.domain} and {self.categorical_filter.domain}.")
         self.categorical_filter = categorical_filter
         self.date_range = date_range
         self.return_date = return_date
