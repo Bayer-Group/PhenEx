80c5fd645f2837336bb98911b265a6d320f1d144
a-hartens <alexander.hartenstein@bayer.com>
2026-03-26 08:42:58 +0100

    adding local file codelist factory init

--- phenex/__init__.py
+++ phenex/__init__.py
@@ -85,6 +85,7 @@ from .reporting import (
 # Codelists
 from .codelists import (
     Codelist,
+    LocalFileCodelistFactory,
     LocalCSVCodelistFactory,
     MedConBCodelistFactory,
     MedConBCollection,
@@ -180,6 +181,7 @@ __all__ = [
     "ReportDrafter",
     # Codelists
     "Codelist",
+    "LocalFileCodelistFactory",
     "LocalCSVCodelistFactory",
     "MedConBCodelistFactory",
     "MedConBCollection",
