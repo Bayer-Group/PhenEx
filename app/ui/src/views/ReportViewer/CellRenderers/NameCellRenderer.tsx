015141a855a518ef6d3a5f67cb33a2227ea49845
a-hartens <alexander.hartenstein@bayer.com>
2026-05-07 08:27:33 +0200

    minor cleanup

--- app/ui/src/views/ReportViewer/CellRenderers/NameCellRenderer.tsx
+++
@@ -1,6 +0,0 @@
-import { FC } from 'react';
-import styles from './NameCellRenderer.module.css';
-
-export const NameCellRenderer: FC<any> = (params) => {
-  return <div className={styles.nameCell}>{params.value}</div>;
-};
