4f50283220c5bfda1f85c685e4f81db156cfad79
a-hartens <alexander.hartenstein@bayer.com>
2026-05-08 14:45:34 +0200

    to static html

---
+++ app/ui/src/views/ReportViewer/static-report.tsx
@@ -0,0 +1,4 @@
+import { createRoot } from 'react-dom/client';
+import { StaticReportViewer } from './StaticReportViewer';
+
+createRoot(document.getElementById('root')!).render(<StaticReportViewer />);
